47025f6af3263feff8f309dd2a8936c2f7b807f1
gita-test <geetha.palyam@gmail.com>
2022-10-25 14:53:50 +0100

    code-refactored

--- index-refactor.py
+++ index-refactor.py
@@ -9,135 +9,144 @@ import boto3
 
 #variables
 #Git UserName
-username = 'geethapalyam'
+USER_NAME = 'geethapalyam'
 #Git Repo Name
-repoName = 'palyam_test'
-#release URL https://api.github.com/repos/{owner}/{repo}/releases/latest
-Rekease_url = "https://api.github.com/repos/actions/runner/releases/latest"
-#yaml File path
-file_path = "runner-test.txt"
-#sender email for the mail notification.
-#sender_email = "geetha.palyam@gmail.com"
-# Gmail app password
-# password = 'kndtknppilckttof'
-
-client = boto3.client('ssm')  # Creates an Amazon Simple Systems Manager client
-sender_email = client.get_parameter(Name='/test/check/sender_email', WithDecryption=True)["Parameter"]["Value"]
-password = client.get_parameter(Name='/test/check/sender_email_password', WithDecryption=True)["Parameter"]["Value"]
-Access_token = client.get_parameter(Name='/test/check/access_token', WithDecryption=True)["Parameter"]["Value"]
-webhook_url = client.get_parameter(Name='/test/check/webhook', WithDecryption=True)["Parameter"]["Value"]
-
-print("sender_email", sender_email)
-print("password", password)
+REPO_NAME = 'palyam_test'
 # Email notification reciever
-receiver_email = "palyamgeetha@gmail.com"
-#branch name
-branch_name = "runner"
+RECEIVER_EMAIL = "palyamgeetha@gmail.com"
 #slack channel receives notification
-channel_name = "#git-script"
+CHANNEL_NAME = "#git-script"
+#release URL https://api.github.com/repos/{owner}/{repo}/releases/latest
+RELEASE_URL = "https://api.github.com/repos/actions/runner/releases/latest"
+#yaml File path
+FILE_PATH = "runner-test.txt"
+#branch name
+BRANCH_NAME = "runner"
+CLIENT = boto3.client('ssm')  # Creates an Amazon Simple Systems Manager client
 
-def sendSlackNotification(pr_number):
+def sendSlackNotification(PR_NUMBER:str):
+    """
+    Arguments:
+            PR_NUMBER {str} -- Pull Request Number
+    """
+    WEBHOOK_URL = CLIENT.get_parameter(Name='/test/check/webhook', WithDecryption=True)["Parameter"]["Value"]
     # Code to send alert to the slack channel
-    url = webhook_url
-    message = ("new Pull Request Link: https://github.com/{}/{}/pull/{}".format(username, repoName, pr_number))
-    title = (f"New Incoming pull Request :zap:")
-    slack_data = {
+    MESSAGE = (f"new Pull Request Link: https://github.com/{USER_NAME}/{REPO_NAME}/pull/{PR_NUMBER}")
+    TITLE = (f"New Incoming pull Request :zap:")
+    SLACK_DATA = {
         "username": "NotificationBot",
         "icon_emoji": ":satellite:",
-        "channel" : channel_name,
+        "channel" : CHANNEL_NAME,
         "attachments": [
             {
                 "color": "#9733EE",
                 "fields": [
                     {
-                        "title": title,
-                        "value": message,
+                        "title": TITLE,
+                        "value": MESSAGE,
                         "short": "false",
                     }
                 ]
             }
         ]
     }
-    byte_length = str(sys.getsizeof(slack_data))
-    headers = {'Content-Type': "application/json", 'Content-Length': byte_length}
-    response = requests.post(url, data=json.dumps(slack_data), headers=headers)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
+    BYTE_LENGTH = str(sys.getsizeof(SLACK_DATA))
+    HEADERS = {'Content-Type': "application/json", 'Content-Length': BYTE_LENGTH}
+    RESPONSE = requests.post(WEBHOOK_URL, data=json.dumps(SLACK_DATA), headers=HEADERS)
+    if RESPONSE.status_code != 200:
+        raise Exception(RESPONSE.status_code, RESPONSE.text)
 
-def sendEmailNotification(pr_number):
+def sendEmailNotification(PR_NUMBER:str):
+    """
+    Arguments:
+            PR_NUMBER {str} -- Pull Request Number
+    """
+    SENDER_EMAIL = CLIENT.get_parameter(Name='/test/check/sender_email', WithDecryption=True)["Parameter"]["Value"]
+    PASSWORD = CLIENT.get_parameter(Name='/test/check/sender_email_password', WithDecryption=True)["Parameter"]["Value"]
     # Code to send alert via email
-    smtp_server = "smtp.gmail.com"
-    port = 587  # For starttls
-
-    url = "https://github.com/{}/{}/pull/{}".format(username, repoName, pr_number)
-    message = '''hello
+    URL = f"https://github.com/{USER_NAME}/{REPO_NAME}/pull/{PR_NUMBER}"
+    MESSAGE = '''hello
 
     A new Pull Request has been created
 
-    The URL is: '''+url
+    The URL is: ''' + URL
 
-    # Create a secure SSL context
-    context = ssl.create_default_context()
+    CONTEXT = ssl.create_default_context()
 
     # Try to log in to server and send email
     try:
-        server = smtplib.SMTP(smtp_server,port)
-        server.ehlo() # Can be omitted
-        server.starttls(context=context) # Secure the connection
-        server.ehlo() # Can be omitted
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        SMTP = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        SMTP.ehlo()
+        SMTP.starttls(context=CONTEXT) # Secure the connection
+        SMTP.ehlo()
+        SMTP.login(SENDER_EMAIL, PASSWORD)
+        SMTP.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, MESSAGE)
     except Exception as e:
         # Print any error messages to stdout
         print(e)
     finally:
-        server.quit()
+        SMTP.quit()
+
+def doGitOperations(ACCESS_TOKEN) -> str:
+    """
+    Arguments:
+        ACCESS_TOKEN {str} -- GitHub Access Token
+    Returns:
+        str -- Created Pull Request Number
+    """
+    GIT = Github(ACCESS_TOKEN)
+    REPO = GIT.get_repo(f"{USER_NAME}/{REPO_NAME}")
+    #Getting the version and checksum of the latest release.
+    RESPONSE = requests.get(RELEASE_URL)
+    #Getting the version and checksum from the Yaml file
+    CONTENTS = REPO.get_contents(FILE_PATH, BRANCH_NAME)
+    RUNNER_VERSION = re.search('runnerVersion="(.*)"', CONTENTS.decoded_content.decode())
+    RUNNER_CHECKSUM = re.search('runnerCheckSum="(.*)"', CONTENTS.decoded_content.decode())
+
+    REPLACED_VERSION = CONTENTS.decoded_content.decode().replace(RUNNER_VERSION.group(1), RESPONSE.json()['name'])
+
+    REPLACED_CHECKSUM = REPLACED_VERSION.replace(RUNNER_CHECKSUM.group(1), re.search('<!-- BEGIN SHA linux-x64 -->(.*)<!-- END SHA linux-x64 -->', str(RESPONSE.json()['body'])).group(1))
+
+    print("changing the version and checksum with the latest release verion and checksum" )
+
+    BLOB1 = REPO.create_git_blob(REPLACED_CHECKSUM, "utf-8")
+    ELEMENT = github.InputGitTreeElement(path=FILE_PATH, mode='100644', type='blob', sha=BLOB1.sha)
+
+    BRANCH_SHA = REPO.get_branch(BRANCH_NAME).commit.sha
+
+    BASE_TREE = REPO.get_git_tree(sha=BRANCH_SHA)
+    TREE = REPO.create_git_tree([ELEMENT], BASE_TREE)
+    PARENT = REPO.get_git_commit(sha=BRANCH_SHA)
+
+    print("commiting the changes to the git repo")
+    COMMIT = REPO.create_git_commit("Version and checksum updated", TREE, [PARENT])
+    BRANCH_REFS = REPO.get_git_ref("heads/"+BRANCH_NAME)
+    BRANCH_REFS.edit(sha=COMMIT.sha)
+
+    print("creating the PR to the changes")
+    PR = REPO.create_pull(title="New pull Request", body="body", head=BRANCH_NAME, base="main")
+
+    return str(PR.number)
 
 def main():
-    g = Github(Access_token)
-    print("check", Access_token)
-    repo = g.get_repo('{}/{}'.format(username, repoName))
+    ACCESS_TOKEN = CLIENT.get_parameter(Name='/test/check/access_token', WithDecryption=True)["Parameter"]["Value"]
+    GIT = Github(ACCESS_TOKEN)
+    print("check", ACCESS_TOKEN)
+    REPO = GIT.get_repo(f"{USER_NAME}/{REPO_NAME}")
     #Getting the version and checksum of the latest release.
-    response = requests.get(Rekease_url)
+    RESPONSE = requests.get(RELEASE_URL)
     #Getting the version and checksum from the Yaml file
-    contents = repo.get_contents(file_path, branch_name)
-    runnerVersion = re.search('runnerVersion="(.*)"', contents.decoded_content.decode())
-    runnerCheckSum = re.search('runnerCheckSum="(.*)"', contents.decoded_content.decode())
-
+    CONTENTS = REPO.get_contents(FILE_PATH, BRANCH_NAME)
+    RUNNER_VERSION = re.search('runnerVersion="(.*)"', CONTENTS.decoded_content.decode())
     print("checking if you are on the latest version")
-    if runnerVersion.group(1) != response.json()['name']:
-        replaced_Version = contents.decoded_content.decode().replace(runnerVersion.group(1), response.json()['name'])
-
-        replaced_Checksum = replaced_Version.replace(runnerCheckSum.group(1), re.search('<!-- BEGIN SHA linux-x64 -->(.*)<!-- END SHA linux-x64 -->', str(response.json()['body'])).group(1))
-
-        print("changing the version and checksum with the latest release verion and checksum" )
-
-        blob1 = repo.create_git_blob(replaced_Checksum, "utf-8")
-        element1 = github.InputGitTreeElement(path=file_path, mode='100644', type='blob', sha=blob1.sha)
-
-        branch_sha = repo.get_branch(branch_name).commit.sha
-
-        base_tree = repo.get_git_tree(sha=branch_sha)
-        tree = repo.create_git_tree([element1], base_tree)
-        parent = repo.get_git_commit(sha=branch_sha)
-
-        print("commiting the changes to the git repo")
-        commit = repo.create_git_commit("Version and checksum updated", tree, [parent])
-        branch_refs = repo.get_git_ref("heads/"+branch_name)
-        branch_refs.edit(sha=commit.sha)
-
-        print("creating the PR to the changes")
-        pr = repo.create_pull(title="New pull Request", body="body", head=branch_name, base="main")
-
+    if RUNNER_VERSION.group(1) != RESPONSE.json()['name']:
+        PR_NUMBER = doGitOperations(ACCESS_TOKEN)
         print("sending notification to the Slack")
-        sendSlackNotification(pr.number)
-
+        sendSlackNotification(PR_NUMBER)
         print("sending notification to the Mail")
-        sendEmailNotification(pr.number)
-
+        sendEmailNotification(PR_NUMBER)
     else:
         print("You are on the latest version")
-
 
 if __name__ == "__main__":
     main()
